f5142b6a282b91411b98cadd220c64f9ef8d9d3a
Fovir <fovir@disroot.org>
2026-08-24 18:06:22 +0800

    feat(mailer): encapsulate `mail.Client`
    Signed-off-by: Fovir <fovir@disroot.org>

---
+++ backend/internal/mailer/client.go
@@ -0,0 +1,157 @@
+package mailer
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/vexgo-org/vexgo/backend/internal/model"
+	"github.com/wneessen/go-mail"
+)
+
+type SMTPMailer interface {
+	LoadConfig(cfg *model.SMTPConfig) error
+	Send(msg Message) error
+	Enabled() bool
+}
+
+// SMTPClient encapsulate `mail.Client` and store SMTP configuration.
+type SMTPClient struct {
+	c      *mail.Client
+	config *model.SMTPConfig
+}
+
+// Message stores mail message in a structural way.
+type Message struct {
+	To       []string
+	Subject  string
+	TextBody string
+	HTMLBody string
+}
+
+// NewClient creates a new `SMTPClient` from a given configuration.
+func NewClient() SMTPMailer {
+	return &SMTPClient{}
+}
+
+// LoadConfig updates the configuration of SMTP client.
+func (c *SMTPClient) LoadConfig(cfg *model.SMTPConfig) error {
+	client, err := createClientFromConfig(cfg)
+	if err != nil {
+		return err
+	}
+
+	c.c = client
+	c.config = cfg
+	return nil
+}
+
+// Send sends a message to recipient(s).
+func (c *SMTPClient) Send(msg Message) error {
+	if err := validateMessge(msg); err != nil {
+		return err
+	}
+
+	message := mail.NewMsg()
+
+	// Construct `from` field of a mail.
+	// If `FromName` is configured, the format will be
+	// FromName <FromEmail>
+	if c.config.FromName != "" {
+		if err := message.FromFormat(
+			c.config.FromName,
+			c.config.FromEmail,
+		); err != nil {
+			return fmt.Errorf("set sender failed: %w", err)
+		}
+	} else {
+		if err := message.From(c.config.FromEmail); err != nil {
+			return fmt.Errorf("set sender failed: %w", err)
+		}
+	}
+
+	if err := message.To(msg.To...); err != nil {
+		return fmt.Errorf("set recipients failed: %w", err)
+	}
+
+	message.Subject(msg.Subject)
+
+	// Set mail body.
+	switch {
+	case msg.TextBody != "" && msg.HTMLBody != "":
+		message.SetBodyString(mail.TypeTextPlain, msg.TextBody)
+		message.AddAlternativeString(mail.TypeTextHTML, msg.HTMLBody)
+
+	case msg.HTMLBody != "":
+		message.SetBodyString(mail.TypeTextHTML, msg.HTMLBody)
+
+	default:
+		message.SetBodyString(mail.TypeTextPlain, msg.TextBody)
+	}
+
+	// Send the message.
+	if err := c.c.DialAndSend(message); err != nil {
+		return fmt.Errorf("send mail failed: %w", err)
+	}
+
+	return nil
+}
+
+func (c *SMTPClient) Enabled() bool {
+	return c.config != nil && c.config.Enabled
+}
+
+// createClientFromConfig is a helper function used to create `mail.Client`
+// from a given `SMTPClientConfig`.
+func createClientFromConfig(cfg *model.SMTPConfig) (*mail.Client, error) {
+	if err := validateConfig(cfg); err != nil {
+		return nil, err
+	}
+
+	client, err := mail.NewClient(
+		cfg.Host,
+		mail.WithPort(cfg.Port),
+		mail.WithSMTPAuth(mail.SMTPAuthAutoDiscover),
+		mail.WithUsername(cfg.Username),
+		mail.WithPassword(cfg.Password),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("create SMTP client failed: %w", err)
+	}
+
+	return client, nil
+}
+
+// validateConfig validates the configuration of SMTP client.
+func validateConfig(cfg *model.SMTPConfig) error {
+	if strings.TrimSpace(cfg.Host) == "" {
+		return errors.New("SMTP host is required")
+	}
+
+	if cfg.Port <= 0 {
+		return errors.New("SMTP port must be greater than zero")
+	}
+
+	if strings.TrimSpace(cfg.FromEmail) == "" {
+		return errors.New("sender address is required")
+	}
+
+	return nil
+}
+
+// validateMessge validates the message to be sent.
+func validateMessge(msg Message) error {
+	if len(msg.To) == 0 {
+		return errors.New("at least one recipient is required")
+	}
+
+	if strings.TrimSpace(msg.Subject) == "" {
+		return errors.New("mail subject is required")
+	}
+
+	if msg.TextBody == "" && msg.HTMLBody == "" {
+		return errors.New("mail body is required")
+	}
+
+	return nil
+}
