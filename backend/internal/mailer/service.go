8805f8af2149769efaba992f4174ae3e79525988
Fovir <fovir@disroot.org>
2026-08-27 16:30:07 +0800

    refactor(mailer): extract a helper to send emails
    Signed-off-by: Fovir <fovir@disroot.org>

--- backend/internal/mailer/service.go
+++ backend/internal/mailer/service.go
@@ -41,85 +41,33 @@ func (s *Service) SendVerificationEmail(
 	toEmail string,
 	data *VerificationEmailTemplateData,
 ) error {
-	if err := s.readConfig(ctx); err != nil {
-		return err
-	}
-
-	htmlBody, err := RenderHTMLTemplate(
-		verificationEmailTemplateHTML,
-		data,
-	)
-	if err != nil {
-		return fmt.Errorf("render html verification email failed: %w", err)
-	}
-
-	textBody, err := RenderTextTemplate(
-		verificationEmailTemplateText,
-		data,
-	)
-	if err != nil {
-		return fmt.Errorf("render text verification email failed: %w", err)
-	}
-
-	const SUBJECT = "Please Verify Your Email Address"
-
-	if mailCaptureHook != nil {
-		mailCaptureHook(toEmail, SUBJECT, textBody, htmlBody)
-		return nil
-	}
-
-	if err := s.client.Send(ctx, Message{
-		To:       []string{toEmail},
-		Subject:  SUBJECT,
-		TextBody: textBody,
-		HTMLBody: htmlBody,
+	if err := s.sendEmail(ctx, sendEmailArgs{
+		ToEmail:      toEmail,
+		Data:         data,
+		Subject:      "Please Verify Your Email Address",
+		TextTemplate: verificationEmailTemplateText,
+		HTMLTemplate: verificationEmailTemplateHTML,
 	}); err != nil {
 		return fmt.Errorf("send verification email failed: %w", err)
 	}
-
 	return nil
 }
 
 // SendPasswordResetEmail sends password reset email
 func (s *Service) SendPasswordResetEmail(
-	ctx context.Context, toEmail string,
+	ctx context.Context,
+	toEmail string,
 	data *PasswordResetEmailTemplateData,
 ) error {
-	if err := s.readConfig(ctx); err != nil {
-		return err
-	}
-
-	textBody, err := RenderTextTemplate(
-		resetPasswordEmailTemplateText,
-		data,
-	)
-	if err != nil {
-		return fmt.Errorf("render text password reset email failed: %w", err)
-	}
-
-	htmlBody, err := RenderHTMLTemplate(
-		resetPasswordEmailTemplateHTML,
-		data,
-	)
-	if err != nil {
-		return fmt.Errorf("render html password reset email failed: %w", err)
-	}
-
-	const SUBJECT = "Password Reset Request"
-	if mailCaptureHook != nil {
-		mailCaptureHook(toEmail, SUBJECT, textBody, htmlBody)
-		return nil
-	}
-
-	if err := s.client.Send(ctx, Message{
-		To:       []string{toEmail},
-		Subject:  SUBJECT,
-		TextBody: textBody,
-		HTMLBody: htmlBody,
+	if err := s.sendEmail(ctx, sendEmailArgs{
+		ToEmail:      toEmail,
+		Data:         data,
+		Subject:      "Password Reset Request",
+		TextTemplate: resetPasswordEmailTemplateText,
+		HTMLTemplate: resetPasswordEmailTemplateHTML,
 	}); err != nil {
 		return fmt.Errorf("send password reset email failed: %w", err)
 	}
-
 	return nil
 }
 
@@ -129,74 +77,33 @@ func (s *Service) SendEmailChangeEmail(
 	toEmail string,
 	data *EmailChangeEmailTemplateData,
 ) error {
-	if err := s.readConfig(ctx); err != nil {
-		return err
-	}
-
-	textBody, err := RenderTextTemplate(emailChangeEmailTemplateText, data)
-	if err != nil {
-		return fmt.Errorf("render text email change email failed: %w", err)
-	}
-
-	htmlBody, err := RenderHTMLTemplate(emailChangeEmailTemplateHTML, data)
-	if err != nil {
-		return fmt.Errorf("render html email change email failed: %w", err)
-	}
-
-	const SUBJECT = "Confirm Email Change"
-
-	if mailCaptureHook != nil {
-		mailCaptureHook(toEmail, SUBJECT, textBody, htmlBody)
-		return nil
-	}
-
-	if err := s.client.Send(ctx, Message{
-		To:       []string{toEmail},
-		Subject:  SUBJECT,
-		TextBody: textBody,
-		HTMLBody: htmlBody,
+	if err := s.sendEmail(ctx, sendEmailArgs{
+		ToEmail:      toEmail,
+		Data:         data,
+		Subject:      "Confirm Email Change",
+		TextTemplate: emailChangeEmailTemplateText,
+		HTMLTemplate: emailChangeEmailTemplateHTML,
 	}); err != nil {
 		return fmt.Errorf("send email change email failed: %w", err)
 	}
-
 	return nil
 }
 
+// SendTestSMTPEmail sends an email to test SMTP configuration.
 func (s *Service) SendTestSMTPEmail(
 	ctx context.Context,
 	toEmail string,
 	data *TestSMTPEmailTemplateData,
 ) error {
-	if err := s.readConfig(ctx); err != nil {
-		return err
-	}
-
-	const SUBJECT = "SMTP Configuration Test Email"
-
-	textBody, err := RenderTextTemplate(testSMTPEmailTemplateText, data)
-	if err != nil {
-		return fmt.Errorf("render text test SMTP email failed: %w", err)
-	}
-
-	htmlBody, err := RenderHTMLTemplate(testSMTPEmailTemplateHTML, data)
-	if err != nil {
-		return fmt.Errorf("render html test SMTP email failed: %w", err)
-	}
-
-	if mailCaptureHook != nil {
-		mailCaptureHook(toEmail, SUBJECT, textBody, htmlBody)
-		return nil
-	}
-
-	if err := s.client.Send(ctx, Message{
-		To:       []string{toEmail},
-		Subject:  SUBJECT,
-		TextBody: textBody,
-		HTMLBody: htmlBody,
+	if err := s.sendEmail(ctx, sendEmailArgs{
+		ToEmail:      toEmail,
+		Data:         data,
+		Subject:      "SMTP Configuration Test Email",
+		TextTemplate: testSMTPEmailTemplateText,
+		HTMLTemplate: testSMTPEmailTemplateHTML,
 	}); err != nil {
-		return fmt.Errorf("send test SMTP email failed: %w", err)
+		return fmt.Errorf("send email change email failed: %w", err)
 	}
-
 	return nil
 }
 
@@ -225,5 +132,49 @@ func (s *Service) readConfig(ctx context.Context) error {
 		return errors.New("SMTP is not enabled")
 	}
 
+	return nil
+}
+
+type sendEmailArgs struct {
+	ToEmail      string
+	Data         any
+	TextTemplate string
+	HTMLTemplate string
+	Subject      string
+}
+
+// sendEmail is a helper function to send emails.
+func (s *Service) sendEmail(
+	ctx context.Context,
+	arg sendEmailArgs,
+) error {
+	if err := s.readConfig(ctx); err != nil {
+		return err
+	}
+
+	textBody, err := RenderTextTemplate(arg.TextTemplate, arg.Data)
+	if err != nil {
+		return fmt.Errorf("render text email failed (template=%v): %w", arg.TextTemplate, err)
+	}
+
+	htmlBody, err := RenderHTMLTemplate(arg.HTMLTemplate, arg.Data)
+	if err != nil {
+		return fmt.Errorf("render html email failed (template=%v): %w", arg.HTMLTemplate, err)
+	}
+
+	// Used for testing.
+	if mailCaptureHook != nil {
+		mailCaptureHook(arg.ToEmail, arg.Subject, textBody, htmlBody)
+		return nil
+	}
+
+	if err := s.client.Send(ctx, Message{
+		To:       []string{arg.ToEmail},
+		Subject:  arg.Subject,
+		TextBody: textBody,
+		HTMLBody: htmlBody,
+	}); err != nil {
+		return err
+	}
 	return nil
 }
